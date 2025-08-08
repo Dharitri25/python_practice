bc50884039042b6040e2b14bd13a38d913086d7b
Dharitri25 <dharitrim.brainerhub@gmail.com>
2025-08-08 19:39:21 +0530

    number and string

--- day1_8.8/1.py
+++ day1_8.8/1.py
@@ -26,3 +26,62 @@
 sdsds'''
 
 # print('j','k')
+
+# data types
+# Text:	str
+# Numeric :	int, float, complex
+# Sequence : list, tuple, range
+# Mapping: dict
+# Set :	set, frozenset
+# Boolean: bool
+# Binary : bytes, bytearray, memoryview
+# None:	NoneType
+
+# number
+# x = 1    # int
+# y = 2.8  # float
+# z = 1j   # complex
+
+# x=1.0
+# print(type(x))
+# y=int(x)
+# print(x,y)
+# complex numbers can not be converted into any other type
+
+# random
+# import random
+# print(random.randrange(1,10))
+
+# string
+# a = "Hello, World!"
+# # print(a[5])
+# print(len(a))
+
+# for x in "banana":
+#   print(x)
+
+x= 'dharitri'
+# print('i' in x)
+# print('i' not in x)
+# print(x[1:4])
+# print(x[:4])
+# print(x[2:])
+# print(x[-5:-2])
+# print(x[::-1])
+# print(x.upper())
+# print(x.lower())
+# print(x.strip())
+# print(x.replace('d','s'))
+# print(x.split(','))
+
+# x='dharitri'
+# y='dharitri'
+# print(x is y)
+# print(x is not y)
+
+# x = "awesome"
+# def myfunc():
+#   global x
+#   x = "fantastic"
+# myfunc()
+# print("Python is " + x)
