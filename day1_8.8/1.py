37042341466b39bb99f2a0e1fac2683cefaa3599
Adity21 <adityverma1987@gmail.com>
2025-08-08 18:54:53 +0530

    first commit

---
+++ day1_8.8/1.py
@@ -0,0 +1,35 @@
+# import sys
+# print(sys.version)
+# print(sys.argv)
+# print("Dharitri")
+
+# syntax 
+# if 2>1:
+#     print("2 is greater than 1")
+# else:
+#     print("2 is not greater than 1")
+
+## Variables
+# type
+# x = 2
+# x='dd'
+# print(x, type(x))
+
+#multiple variables
+# x,y,z=1,'d2',3.0
+# print(x,y,z)
+
+# x=y=z= '1'
+# print(x,y,z)
+
+'''print('sdsdsdsd')
+sdsds'''
+
+# print('j','k')
+echo "# python_practice" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Dharitri25/python_practice.git
+git push -u origin main
